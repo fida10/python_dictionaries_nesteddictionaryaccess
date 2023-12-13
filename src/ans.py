8ec8be067fdf21ae571b235d3318ff6a4fd91913
eshad <fidaeshad10@gmail.com>
2023-12-12 18:05:05 -0600

    answered

--- src/ans.py
+++ src/ans.py
@@ -8,3 +8,13 @@ and a list of keys. The function should return the value located by
 following the sequence of keys in the nested dictionary. 
 If any key is not found at any level, return None.
 """
+
+def get_nested_value(dict, list):
+    if len(list) == 0:
+        return None
+    try:
+        for indiv_element in list:
+            dict = dict[indiv_element]
+        return dict
+    except KeyError:
+        return None
